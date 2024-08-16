89ad7326ab20ae8da5cc25835e05442126f07dcf
Carlos Cabral <carloscabral@google.com>
2024-08-16 10:29:10 -0300

    feat: more stuff

--- logging_template/deploy.sh
+++ logging_template/deploy.sh
@@ -5,7 +5,7 @@ apigeecli prefs set -o $project
 token=$(gcloud auth print-access-token)
 apigeecli token cache -t $token
 ENV=dev
-PROXY_NAME=novo-proxy
+PROXY_NAME=unico-v2
 SA=apigee-proxy-service-account@cabral-apigee.iam.gserviceaccount.com
 
 apigeecli apis create bundle -e $ENV -n $PROXY_NAME --sa $SA -p newproxy.zip --wait --ovr
